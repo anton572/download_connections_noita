bdf6cba0105709c39c10910962fe66798bb9e555
anton572 <135094776+anton572@users.noreply.github.com>
2024-05-24 17:41:31 +0300

    update impotrt

--- importer.py
+++ importer.py
@@ -1,3 +1,12 @@
-
+import subprocess
+import sys
 def im(name):
-    return __import__(name)
+    try:
+        return __import__(name)
+    except ImportError:
+        try:
+            subprocess.check_call([sys.executable, "-m", "pip", "install", name])
+            return __import__(name)
+        except Exception as e:
+            print(f"Failed to install and import {name}: {e}")
+            return None
